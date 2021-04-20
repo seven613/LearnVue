6059bdfc29f5121178742be77f5dc599911d62e1
seven613 <seven613@126.com>
2021-04-20 21:55:28 +0800

    tabbar init

---
+++ day07/tabbar/src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="tab-bar-item">
+    <slot name="item-icon" ></slot>
+    <slot name="item-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+};
+</script>
+
+<style scoped>
+.tab-bar-item {
+flex:1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px
+}
+.tab-bar-item img {
+  height: 24px;
+  width: 24px;
+  margin-top:3px;
+  vertical-align: middle;
+  margin-bottom: 2px;
+}
+</style>
